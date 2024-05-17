ba1d03e474790d6936a87a9322b74c910fc9e901
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2024-05-17 21:04:32 +0300

    Initial commit

---
+++ src/components/home/BestSellers/BestSellers.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Heading from "../Products/Heading";
+import Product from "../Products/Product";
+import {
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+} from "../../../assets/images/index";
+
+const BestSellers = () => {
+  return (
+    <div className="w-full pb-20">
+     
+    </div>
+  );
+};
+
+export default BestSellers;
